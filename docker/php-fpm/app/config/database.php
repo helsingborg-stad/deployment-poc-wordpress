28e3d2ab9545177d0d50f77d3a8bdb16a636a4aa
Joel Bernerman <joel.bernerman@helsingborg.se>
2021-06-24 12:06:45 +0200

    Use full json in conf instead because cfn sux

--- docker/php-fpm/app/config/database.php
+++ docker/php-fpm/app/config/database.php
@@ -1,16 +1,17 @@
 <?php
 
+$wpSecrets = json_decode(getenv('WP_SECRETS'), true);
 
 // ** MySQL settings - You can get this info from your web host ** //
 
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('WP_DB_NAME'));
+define('DB_NAME', $wpSecrets['WP_DB_NAME']);
 
 /** MySQL database username */
-define('DB_USER', getenv('WP_DB_USER'));
+define('DB_USER', $wpSecrets['WP_DB_USER']);
 
 /** MySQL database password */
-define('DB_PASSWORD', getenv('WP_DB_PASSWORD'));
+define('DB_PASSWORD', $wpSecrets['WP_DB_PASSWORD']);
 
 /** MySQL hostname */
 define('DB_HOST', getenv('WP_DB_HOST'));
--- docker/php-fpm/app/config/salts.php
+++ docker/php-fpm/app/config/salts.php
@@ -1,10 +1,12 @@
 <?php
 
-define('AUTH_KEY', getenv('WP_AUTH_KEY'));
-define('SECURE_AUTH_KEY', getenv('WP_SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY', getenv('WP_LOGGED_IN_KEY'));
-define('NONCE_KEY', getenv('WP_NONCE_KEY'));
-define('AUTH_SALT', getenv('WP_AUTH_SALT'));
-define('SECURE_AUTH_SALT', getenv('WP_SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT', getenv('WP_LOGGED_IN_SALT'));
-define('NONCE_SALT', getenv('WP_NONCE_SALT'));
+$wpSecrets = json_decode(getenv('WP_SECRETS'), true);
+
+define('AUTH_KEY', $wpSecrets['WP_AUTH_KEY']);
+define('SECURE_AUTH_KEY', $wpSecrets['WP_SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $wpSecrets['WP_LOGGED_IN_KEY']);
+define('NONCE_KEY', $wpSecrets['WP_NONCE_KEY']);
+define('AUTH_SALT', $wpSecrets['WP_AUTH_SALT']);
+define('SECURE_AUTH_SALT', $wpSecrets['WP_SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT', $wpSecrets['WP_LOGGED_IN_SALT']);
+define('NONCE_SALT', $wpSecrets['WP_NONCE_SALT']);
