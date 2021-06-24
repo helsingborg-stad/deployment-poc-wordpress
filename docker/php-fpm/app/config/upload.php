6f0d9224f3df3b88ea6353e5e777026f8336d1c5
Joel Bernerman <joel.bernerman@helsingborg.se>
2021-06-24 14:54:32 +0200

    Added s3 uploads region

--- docker/php-fpm/app/config/upload.php
+++ docker/php-fpm/app/config/upload.php
@@ -13,3 +13,5 @@ define('WP_MEMORY_LIMIT', '512M');
 define('S3_UPLOADS_BUCKET', getenv('S3_UPLOADS_BUCKET'));
 define('S3_UPLOADS_KEY', getenv('S3_UPLOADS_KEY'));
 define('S3_UPLOADS_SECRET', getenv('S3_UPLOADS_SECRET'));
+define('S3_UPLOADS_REGION', getenv('S3_UPLOADS_REGION'));
+
