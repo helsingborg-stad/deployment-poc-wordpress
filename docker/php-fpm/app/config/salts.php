6311190d1112e3fb6e93e76d242ac5304d1d7fd4
Joel Bernerman <joel.bernerman@helsingborg.se>
2021-06-28 13:46:32 +0200

    Use ssm instead of secrets so we can get env vars working

--- docker/php-fpm/app/config/salts.php
+++ docker/php-fpm/app/config/salts.php
@@ -1,12 +1,10 @@
 <?php
 
-$wpSecrets = json_decode(getenv('WP_SECRETS'), true);
-
-define('AUTH_KEY', $wpSecrets['WP_AUTH_KEY']);
-define('SECURE_AUTH_KEY', $wpSecrets['WP_SECURE_AUTH_KEY']);
-define('LOGGED_IN_KEY', $wpSecrets['WP_LOGGED_IN_KEY']);
-define('NONCE_KEY', $wpSecrets['WP_NONCE_KEY']);
-define('AUTH_SALT', $wpSecrets['WP_AUTH_SALT']);
-define('SECURE_AUTH_SALT', $wpSecrets['WP_SECURE_AUTH_SALT']);
-define('LOGGED_IN_SALT', $wpSecrets['WP_LOGGED_IN_SALT']);
-define('NONCE_SALT', $wpSecrets['WP_NONCE_SALT']);
+define('AUTH_KEY', getenv('WP_AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('WP_SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('WP_LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('WP_NONCE_KEY'));
+define('AUTH_SALT', getenv('WP_AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('WP_SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('WP_LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('WP_NONCE_SALT'));
